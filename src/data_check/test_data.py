b20edb3caabcfc84ea6fc0eed4e3a0f20474cc92
misa <sampae2402@hotmail.com>
2021-12-25 11:03:58 +0100

    pipeline: added steps before random forest training

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Test for reasonable dataset size
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Checks if the prices in the data are betwin min_price and max_price
+    """
+    assert data['price'].between(min_price,max_price).all()
+
